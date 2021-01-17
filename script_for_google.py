a010708971e90c38e2fc203f093114a0d68ed672
tarekrahman3 <44648395+tarekrahman3@users.noreply.github.com>
2021-01-17 11:22:30 +0600

    Update script_for_google.py

--- script_for_google.py
+++ script_for_google.py
@@ -1,60 +1,59 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import csv
 import os
 
 options = Options()
-options.headless = True
+# options.headless = True
 options.add_argument("--no-sandbox")
 options.add_experimental_option("useAutomationExtension", False)
 options.add_experimental_option("excludeSwitches",["enable-automation"])
 options.add_argument("--start-maximized")
 options.add_argument('--ignore-certificate-errors')
+index0=[]
+index1=[]
+index2=[]
 
-text = []
-url = []
-rows = []
-
-driver = webdriver.Chrome(options=options, executable_path='/home/tarek/Selenium Projects/Google Search Result First Link/chromedriver')
-Strings = []
-
-with open('Strings_import_data.csv', 'r') as file:
+csvinput=[]
+driver = webdriver.Chrome(options=options, executable_path='/home/practice_environment/chromedriver')
+strings=[]
+with open('strings_import_data.csv', 'r') as file:
     reader = csv.reader(file)
     for csvline in reader:
-	    data = csvline
-	    csvinput = '\n'.join(data)
-	    Strings.append(csvinput)
-
-for string in Strings:
+        strings.append(csvline)
+for string in strings:
+    index0.append(string)
+    time.sleep(1)
+    driver.get("https://www.google.com")
     time.sleep(3)
-    driver.get('http://www.google.com')
-    time.sleep(4)
     que = driver.find_element_by_xpath("//input[@name='q']")
     que.send_keys("site:linkedin.com/company/ ", string)
-    time.sleep(4)
     que.send_keys(Keys.RETURN)
-    time.sleep(8)
-    
+    time.sleep(5)
     try:
-	col__1 = driver.find_element(By.ID, "rso").find_element_by_xpath('.//span[1]').text
+        titles = driver.find_elements_by_xpath("//div[@class='g']")
+        for each_title in titles:
+            title = each_title.find_element_by_xpath(".//div[@class='yuRUbf']/a/h3").text
     except:
-	col__1 = 'N/A'
-    col_1 = ['Title:', col__1]
-    text.append(col_1)
-    
+        title = 'could not locate'
+    index1.append(title)
     try:
-        col__2 = driver.find_element(By.ID, "rso").find_element_by_xpath('.//a').get_attribute('href')
+        urls = driver.find_elements_by_xpath("//div[@class='g']")
+        for each_url in urls:
+            url = each_url.find_element_by_xpath(".//div[@class='yuRUbf']/a").get_attribute('href')
     except:
-	col__1 = 'N/A'
-    col_2 = ['Website:', col__2]
-    url.append(col_2)
-    row = [col_1, col_2]
-    rows.append(row)
-    time.sleep(4)
-
-for data in rows:
-    print(data)
-
+        url = 'could not locate'
+    index2.append(url)
+    time.sleep(2)
+    print(f"Completed: {string}")
 driver.quit()
+data = {'string':  index0,
+        'title':  index1,
+        'url': index2
+        }
+df = pd.DataFrame (data, columns = ['string','title','url'])
+df.to_csv (r'GoogleSearch_export_data.csv', index = False, header=True)
+print (df)
